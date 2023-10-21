41acf4c590b5a9e67c4238ae3e7f3a77fb1ae5dc
Fabio Gonzalez <fabiiogonzalez8@gmail.com>
2023-10-21 17:55:26 +0200

    modificación

--- crear_cliente.sh
+++ crear_cliente.sh
@@ -13,11 +13,23 @@ nombrered="$3"
 
 # Plantilla
 
-# Línea de creación del disco enlazado
+# Crear nuevo volumen
 
 echo "Creando disco enlazado"
 
-virsh -c qemu:///system vol-create-as default $nombremaquina.qcow2 $tamadisco --format qcow2 --backing-vol plantilla-cliente.qcow2 --backing-vol-format qcow2
+virsh -c qemu:///system vol-create-as default $nombremaquina.qcow2 "${tamadisco}G" --format qcow2 --backing-vol plantilla-cliente.qcow2 --backing-vol-format qcow2
+
+# Cambiar el hostname
+
+sudo virt-customize -c qemu:///system -a /var/lib/libvirt/images/$nombremaquina.qcow2 --hostname $nombremaquina
+
+# Redimension
+
+cp /var/lib/libvirt/images/$nombremaquina.qcow2 /var/lib/libvirt/images/nuevo$nombremaquina.qcow2
+
+virt-resize --expand /dev/sda1 /var/lib/libvirt/images/nuevo$nombremaquina.qcow2 /var/lib/libvirt/images/$nombremaquina.qcow2
+
+sudo rm /var/lib/libvirt/images/nuevo$nombremaquina.qcow2
 
 # Creación de máquina
 
@@ -28,13 +40,16 @@ virt-install --connect qemu:///system \
 			 --virt-type kvm \
 			 --name $nombremaquina \
 			 --os-variant debian10 \
-			 --disk path=/var/lib/libvirt/images/$nombremaquina.qcow2 \
+			 --disk path=/var/lib/libvirt/images/$nombremaquina.qcow2,size=$tamadisco,format=qcow2 \
 			 --memory 1024 \
 			 --vcpus 1 \
-			 --import
+			 --import \
+			 --network bridge=$nombrered
 
 # Conexión a la máquina
 
 echo "Conectando a máquina"
 
 virt-viewer -c qemu:///system $nombremaquina
+
+echo "La máquina $nombremaquina se ha creado con la red $nombrered."
